df45dfa6f3a2549cf48cd4358b0ff8477305df98
Maximino Peralta <maximino.peralta5791@gmail.com>
2016-10-19 11:40:32 -0500

    Update check_arrays.js

--- check_arrays.js
+++ check_arrays.js
@@ -1,10 +1,17 @@
 var Check_arrays = function(){
-        /*check if exist a item of array1 within array2*/
+        /*check if exist a item of array2 within array1*/
         function exist(array1, array2){
-        
+                return array2.some( function (item_array2){
+                        return array1.indexOf( item_array2 ) >= 0;
+                });
         }
         
         return{
+                existe:exist
         }
         
 }();
+
+$(function(){
+        Check_arrays.existe();
+});
