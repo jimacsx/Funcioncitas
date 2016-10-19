97c4a618dc94bd43f7c1ed6fcd07a1d261b0863e
Maximino Peralta <maximino.peralta5791@gmail.com>
2016-10-19 11:34:46 -0500

    Create check_arrays.js

---
+++ check_arrays.js
@@ -0,0 +1,10 @@
+var Check_arrays = function(){
+        /*check if exist a item of array1 within array2*/
+        function exist(array1, array2){
+        
+        }
+        
+        return{
+        }
+        
+}();
